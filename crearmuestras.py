5cdb5cbd8979678af29d2df7a4bfd74e171ba5c6
GerardDLRG <130602559+GerardDLRG@users.noreply.github.com>
2023-04-12 21:35:00 +0200

    Add files via upload

---
+++ crearmuestras.py
@@ -0,0 +1,95 @@
+#LIBRERIAS Y DEPENDENCIAS
+import cv2
+import numpy as np
+import os
+from matplotlib import pyplot as plt
+import time
+import mediapipe as mp
+
+#CONSEGUIR PUNTOS DE REFERENCIA Y VISUALIZARLOS CON MEDIAPIPE Y CV2
+
+mp_holistic = mp.solutions.holistic # Para que mediapipe leea los puntos de referencia desde la camara
+mp_drawing = mp.solutions.drawing_utils # Para ver por pantalla los puntos de referencia que mediapìpe esta leyendo en la variable anterior
+
+def mediapipe_detection(image, model):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # conversión de color del frame leido de la camara a uno con el que mediapipe pueda trabajar
+    image.flags.writeable = False                  # Freezeo de la imagen para hacer la predicción
+    results = model.process(image)                 # Predicción 
+    image.flags.writeable = True                   # Descongelar imagen una vez hecha la prediccón 
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) # Desacemos la conversión de color anterior
+    return image, results
+
+def draw_landmarks(image, results):
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS) # Pinta las conexiones de la pose 
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS) # Pinta las conexiones sobre la imagen NO por pantalla para pintar por pantala se usa matplotlib
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS) # Pinta las conexiones aobre la imagen NO por pantalla
+
+#EXTRAER LOS PUNTOS DE REFERENCIA Y CONEXIONES DE MEDIAPIPE PARA ANALISIS
+def extract_keypoints(results):
+    pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33*4)
+    lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
+    rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
+    return np.concatenate([pose, lh, rh])
+
+#CREAR LAS CARPETAS PARA ALMACENAR LOS DATOS
+# Direccion para guardar la carpetas
+DATA_PATH = os.path.join('MP_Data') 
+# Acciones que la IA ha de detectar
+actions = np.array(['ven', 'para', 'sientate'])
+# 30 videos de 30 frames de duracion para cada accion 
+no_sequences = 30
+sequence_length = 30
+start_folder = 30
+#BUCLE PARA CREAR LAS CARPETAS AUTOMATICAMENTE
+for action in actions: 
+    for sequence in range(0,no_sequences+1):
+        try: 
+            os.makedirs(os.path.join(DATA_PATH, action, str(sequence)))
+        except:
+            pass
+
+#CAPTURA DE DATOS USANDO LAS FUNCIONES ANTERIORES, CON MEDIAPIPE Y OPENCV
+#Declarar que camara usar, ir probrando en caso de que la camara deseada no este en la localización 0
+cap = cv2.VideoCapture(0)
+# Declarar modelo de MediaPipe 
+with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+    # Bucle a traves de las diferentes acciones definidas antes (se pueden añadir mas acciones al vector sin causar fallos)
+    for action in actions:
+        # Bucle para capturar numero de videos declarado en variable no_secuences
+        for sequence in range(no_sequences):
+            # Bucle para capturar los frames en sequence_lenght, para cada video
+            for frame_num in range(sequence_length):
+
+                # Leer camara
+                ret, frame = cap.read()
+                # Detectar landmarks con MediaPipe
+                image, results = mediapipe_detection(frame, holistic)
+                # Pintar las detecciones por pantalla para que queda mejor visualmente
+                draw_landmarks(image, results)
+                # Esperars para la captura de datos
+                if frame_num == 0: 
+                    cv2.putText(image, 'STARTING COLLECTION', (120,200), 
+                               cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255, 0), 4, cv2.LINE_AA)
+                    cv2.putText(image, 'Collecting frames for {} Video Number {}'.format(action, sequence), (15,12), 
+                               cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+                    # Show to screen
+                    cv2.imshow('OpenCV Feed', image)
+                    cv2.waitKey(500)
+                else: 
+                    cv2.putText(image, 'Collecting frames for {} Video Number {}'.format(action, sequence), (15,12), 
+                               cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+                    # Show to screen
+                    cv2.imshow('OpenCV Feed', image)
+                # Extraer los datos en las carpetas declaradas antes
+                keypoints = extract_keypoints(results)
+                npy_path = os.path.join(DATA_PATH, action, str(sequence), str(frame_num))
+                np.save(npy_path, keypoints)
+                # Break gracefully
+                if cv2.waitKey(10) & 0xFF == ord('q'):
+                    break
+    #Al acabar cerrar todas las instancias de openCV y la camara                
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+
---
+++ prueba.py
@@ -0,0 +1,61 @@
+import cv2
+import numpy as np
+from keras.models import load_model
+
+# Cargar el modelo entrenado
+model = load_model('action.h5')
+
+# Inicializar la cámara
+cap = cv2.VideoCapture(0)
+
+# Dimensiones de la imagen de entrada del modelo
+img_width, img_height = 128, 128
+
+# Mapeo de las clases de salida del modelo a acciones del robot
+actions = np.array(['ven', 'para', 'sientate'])
+
+# Función para preprocesar la imagen
+def preprocess_image(img):
+    img = cv2.resize(img, (img_width, img_height))
+    img = np.array(img)
+    img = img.astype('float32')
+    img /= 255
+    img = np.expand_dims(img, axis=0)
+    return img
+
+# Loop principal
+while True:
+    # Capturar una imagen de la cámara
+    ret, frame = cap.read()
+    
+    # Preprocesar la imagen
+    processed_image = preprocess_image(frame)
+    
+    # Realizar la predicción
+    prediction = model.predict(processed_image)
+    predicted_class = np.argmax(prediction)
+    
+    # Obtener la acción correspondiente a la predicción
+    action = actions[predicted_class]
+    
+    # Ejecutar la acción en el robot
+    if action == 'Ven':
+        # Mover el robot hacia adelante
+        pass
+    elif action == 'Para':
+        # Mover el robot hacia atrás
+        pass
+    elif action == 'Sientate':
+        # Mover el robot hacia la izquierda
+        pass
+        
+    # Mostrar la imagen en la pantalla
+    cv2.imshow('Capturando Imagen', frame)
+    
+    # Esperar a que se presione la tecla 'q' para salir del loop
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Liberar los recursos
+cap.release()
+cv2.destroyAllWindows()
