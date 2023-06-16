7f2b405a4a9864080e3ab541fc33b787cf6d895f
Gerard de la rosa garcia <gerarddlrg@gmail.com>
2023-06-16 15:57:47 +0200

    hola

---
+++ ModelFIrstTries/pruebaentiemporeal.py
@@ -0,0 +1,157 @@
+import cv2
+import numpy as np
+import os
+from matplotlib import pyplot as plt
+import time
+import mediapipe as mp
+from scipy import stats
+import tensorflow
+import keras
+import sklearn
+import os
+import numpy as np
+from sklearn.model_selection import train_test_split
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import LSTM, Dense
+from keras.callbacks import TensorBoard
+import time
+
+
+DATA_PATH = os.path.join('MP_Data')
+actions = np.array(['ven', 'para', 'sientate'])
+sequence_length = 30
+mp_holistic = mp.solutions.holistic # Para que mediapipe leea los puntos de referencia desde la camara
+mp_drawing = mp.solutions.drawing_utils # Para ver por pantalla los puntos de referencia que mediapìpe esta leyendo en la variable anterior
+colors = [(245,117,16), (117,245,16), (16,117,245)]
+
+def forward(speed=100):
+    #dataCMD = json.dumps({'var': "move", 'val': 1})
+    #ser.write(dataCMD.encode())
+    print('robot-forward')
+
+
+def steadyMode():
+    #dataCMD = json.dumps({'var': "funcMode", 'val': 1})
+    #ser.write(dataCMD.encode())
+    print('robot-steady')
+
+
+def handShake():
+    #dataCMD = json.dumps({'var': "funcMode", 'val': 3})
+    #ser.write(dataCMD.encode())
+    print('robot-handshake')
+
+last_action_time = time.time()
+min_time_between_actions = 3  # Minimum time difference in seconds
+
+def prob_viz(res, actions, input_frame, colors):
+    output_frame = input_frame.copy()
+    for num, prob in enumerate(res):
+        cv2.rectangle(output_frame, (0,60+num*40), (int(prob*100), 90+num*40), colors[num], -1)
+        cv2.putText(output_frame, actions[num], (0, 85+num*40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
+        
+    return output_frame
+def mediapipe_detection(image, model):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # conversión de color del frame leido de la camara a uno con el que mediapipe pueda trabajar
+    image.flags.writeable = False                  # Freezeo de la imagen para hacer la predicción
+    results = model.process(image)                 # Predicción 
+    image.flags.writeable = True                   # Descongelar imagen una vez hecha la prediccón 
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) # Desacemos la conversión de color anterior
+    return image, results
+
+def draw_landmarks(image, results):
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS) # Pinta las conexiones de la pose 
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS) # Pinta las conexiones sobre la imagen NO por pantalla para pintar por pantala se usa matplotlib
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS) # Pinta las conexiones aobre la imagen NO por pantalla
+
+#EXTRAER LOS PUNTOS DE REFERENCIA Y CONEXIONES DE MEDIAPIPE PARA ANALISIS
+def extract_keypoints(results):
+    pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33*4)
+    lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
+    rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
+    return np.concatenate([pose, lh, rh])
+
+model = Sequential()
+model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(30,258)))
+model.add(LSTM(128, return_sequences=True, activation='relu'))
+model.add(LSTM(64, return_sequences=False, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(actions.shape[0], activation='softmax'))
+model.load_weights('action.h5')
+    
+# 1. New detection variables
+sequence = []
+sentence = []
+predictions = []
+threshold = 0.7
+
+cap = cv2.VideoCapture(0)
+# Set mediapipe model 
+with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("Error: Could not receive frame.")
+            break
+
+        # Read feed
+        ret, frame = cap.read()
+
+        # Make detections
+        image, results = mediapipe_detection(frame, holistic)
+        print(results)
+        
+        # Draw landmarks
+        draw_landmarks(image, results)
+        
+        # 2. Prediction logic
+        keypoints = extract_keypoints(results)
+        sequence.append(keypoints)
+        sequence = sequence[-30:]
+        
+        if len(sequence) == 30:
+            res = model.predict(np.expand_dims(sequence, axis=0))[0]
+            detected_action = actions[np.argmax(res)]
+            print(detected_action)
+            predictions.append(np.argmax(res))
+
+            current_time = time.time()
+            if len(sentence) > 0 and detected_action != sentence[-1] and (current_time - last_action_time) > min_time_between_actions:
+                if detected_action == 'ven':
+                    forward()
+                elif detected_action == 'para':
+                    steadyMode()
+                elif detected_action == 'sientate':
+                    handShake()
+                last_action_time = current_time
+            
+        #3. Viz logic
+            if np.unique(predictions[-10:])[0]==np.argmax(res): 
+                if res[np.argmax(res)] > threshold: 
+                    
+                    if len(sentence) > 0: 
+                        if actions[np.argmax(res)] != sentence[-1]:
+                            sentence.append(actions[np.argmax(res)])
+                    else:
+                        sentence.append(actions[np.argmax(res)])
+
+            if len(sentence) > 5: 
+                sentence = sentence[-5:]
+
+            # Viz probabilities
+            image = prob_viz(res, actions, image, colors)
+            
+        cv2.rectangle(image, (0,0), (640, 40), (245, 117, 16), -1)
+        cv2.putText(image, ' '.join(sentence), (3,30), 
+                       cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+        
+        # Show to screen
+        cv2.imshow('OpenCV Feed', image)
+
+        # Break gracefully
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
---
+++ ModelLSTMcon100muestras/crearIA.py
@@ -0,0 +1,62 @@
+#Librerias e imports
+import tensorflow
+import keras
+import sklearn
+import os
+import numpy as np
+from sklearn.model_selection import train_test_split
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import LSTM, Dense,  TimeDistributed, Conv2D, MaxPooling2D, Dropout, Flatten, Conv1D, MaxPooling1D
+from keras.callbacks import TensorBoard
+from sklearn.metrics import multilabel_confusion_matrix, accuracy_score
+#Vuelvo a declarar las varibles de actions sequences y datapath, en lugar de importarlas del otro archivo, para evitar lanzarlo mas de una vez porq eso hara que se sobreescriban los datos ya recogidos 
+DATA_PATH = os.path.join('MP_Data_100')
+actions = np.array(['ven', 'para', 'sientate'])
+sequence_length = 30
+
+#ORDENAR LOS DATOS RECOGIDOS DE FORMA QUE TENSORFLOW PUEDA TRABAJAR CON ELLOS
+label_map = {label:num for num, label in enumerate(actions)} # ven:0, para:1, sientate:2
+sequences, labels = [], [] #Secuencias actuan como X, labels como Y de una funcion, la IA ajustara el resto de parametros para hacer que cada X lleve a su Y correspondiente
+for action in actions:
+    for sequence in np.array(os.listdir(os.path.join(DATA_PATH, action))).astype(int):
+        window = []
+        for frame_num in range(sequence_length): #Para cada uno de los frames
+            res = np.load(os.path.join(DATA_PATH, action, str(sequence), "{}.npy".format(frame_num))) #Cargar los datos de numpy de los directorios
+            window.append(res) #Mete cada frame separado en una ventana conjunta
+        sequences.append(window) #Junta todos los videos/ventanas en un vector que contiene todos los videos de manea ordenada
+        labels.append(label_map[action]) #Junta todos los nombres de la actions en un vector de manera ordena
+#RESULTADO --> secuences[0] = video0Ven , labels[0] = 0 , ... , secuences[30] = video0Para , labels[30] = 1 , ... , 
+
+X = np.array(sequences)
+print(X.shape)
+Y = to_categorical(labels).astype(int) #De int a binario
+#Particiones de crear modelo y testear modelo
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+
+#LSTM 
+#logs de como se esta entrenando la IA en tiempo real, se pueden ver en web
+log_dir = os.path.join('Logs_100')
+tb_callback = TensorBoard(log_dir=log_dir)
+
+model = Sequential()
+model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(30,258)))
+model.add(LSTM(128, return_sequences=True, activation='relu'))
+model.add(LSTM(64, return_sequences=False, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(actions.shape[0], activation='softmax'))
+
+
+model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
+model.fit(X_train, Y_train, epochs=35, validation_data=(X_test, Y_test), callbacks=[tb_callback])
+model.save('actionCon100.h5')
+model.summary()
+
+yhat = model.predict(X_test)
+ytrue = np.argmax(Y_test, axis=1).tolist()
+yhat = np.argmax(yhat, axis=1).tolist()
+confusion = multilabel_confusion_matrix(ytrue, yhat)
+accuracy = accuracy_score(ytrue, yhat)
+print(confusion)
+print(accuracy)
